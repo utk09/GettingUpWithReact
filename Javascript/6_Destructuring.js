ac82461e712ac88f11507bc67c2a657d60c5be31
utk09 <utkarsh.tiwari@somaiya.edu>
2019-01-13 19:43:00 +0530

    Added Destructuring

---
+++ Javascript/6_Destructuring.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 3, 4];
+[num1, num2, num3] = numbers;
+console.log(num1, num2, num3);
+
+console.log("---------------------------------");
+
+numbers = [1, 3, 5, 7];
+[num1, , , num4] = numbers;
+console.log(num1, num4);
