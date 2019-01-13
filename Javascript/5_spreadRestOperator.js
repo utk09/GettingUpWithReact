d83c975191048ec88eedf698793007f0e880e34b
utk09 <utkarsh.tiwari@somaiya.edu>
2019-01-13 19:17:12 +0530

    Updated

--- Javascript/5_spreadRestOperator.js
+++ Javascript/5_spreadRestOperator.js
@@ -4,6 +4,21 @@ console.log(newNumber);
 
 console.log("----------------------------------------")
 
-const number2 = [1, 2, 3, 4]; 
+const number2 = [1, 2, 3, 4];
 const newNumber2 = [number2, 8, 7, 6, 5]; // "number2" takes values from "number2" array and then get's appended to the numbers in "newNumber2" array as an array rather than continuation
 console.log(newNumber2);
+
+console.log("----------------------------------------")
+console.log("----------------------------------------")
+
+
+const person = {
+    name: "Hulk"
+};
+
+const personAge = {
+    ...person,
+    age: 24
+};
+
+console.log(personAge);
