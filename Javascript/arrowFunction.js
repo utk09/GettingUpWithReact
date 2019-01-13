309401f8cae87449c845305527d10825b08cb174
utk09 <utkarsh.tiwari@somaiya.edu>
2019-01-13 11:42:10 +0530

    first commit

---
+++ Javascript/arrowFunction.js
@@ -0,0 +1,42 @@
+function myName(name) { // way to declare a function
+    console.log(name); 
+}
+
+myName('Madmax'); // calling a function
+
+console.log("---------------------------------------")
+
+const myAnotherName = (name) => { // let and const are two ways to define a variable, "var" is not used from ES6 now
+    console.log(name);
+}
+
+myAnotherName('Fury');
+
+
+console.log("---------------------------------------")
+
+let twoArguments = (name, age) => {
+    console.log("My name:", name + "\t My age:", age); // "\t" is an escape character, it gives a spacing of a "tab" key. Another important escape character is "\n", takes things to "new line".
+}
+
+twoArguments('Holaa', 45);
+/* 
+This is the way to write multi-line comment in Javascript.
+This is one line.
+This is another line.
+This is one another line.
+*/
+
+console.log("---------------------------------------")
+
+const mutiplyNums = number => {
+    return number * number * 3;
+}
+
+console.log("Number * Number * 3: ", mutiplyNums(4));
+
+
+console.log("---------------------------------------")
+
+const singleLine = (number, anotherNum) => number * anotherNum;
+console.log("number * anotherNum: ", singleLine(3, 6));
---
+++ Javascript/variables.js
@@ -0,0 +1,9 @@
+console.log(674); // console.log is like "print" statement, it prints output on the DEBUG CONSOLE of VSCode. Shortcut to Debug, Press F5 key.
+
+let myName = "utk09";
+console.log(myName); 
+// Anything with "//" is a comment, it doesn't get executed.
+
+myName = "Be Batman or IronMan";
+console.log(myName);
+// "var", "let", "const" are three ways to declare variables.
