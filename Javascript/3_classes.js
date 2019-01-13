ca313326db424409b6da8bc621652a3a3a78ca0d
utk09 <utkarsh.tiwari@somaiya.edu>
2019-01-13 12:43:48 +0530

    Update inheritance

--- Javascript/3_classes.js
+++ Javascript/3_classes.js
@@ -13,3 +13,23 @@ class Person { //  creating a class named "Person"
 
 const person = new Person(); // assignning class to an instance of class
 person.printMyValues(); // calling instance
+
+// Let us now see inheritance of classes
+
+console.log("-------------------------------------")
+
+class IronMan extends Person { // extends is a keyword
+    constructor() {
+        super(); // super is special way to call the parent class from where values are inherited
+        this.version = "Mark 42";
+        //  this.age = 67; // Uncomment this when you run this for the second time, it actually assigns new values to "age"
+    }
+
+    printVersion() {
+        console.log(this.version);
+    }
+}
+
+const ironman = new IronMan();
+ironman.printMyValues();
+ironman.printVersion();
