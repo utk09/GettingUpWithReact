e2d00d43ee9f1904ff1d401e6aaafe3fc7696c9b
utk09 <utkarsh.tiwari@somaiya.edu>
2019-01-13 19:01:35 +0530

    Added spread and sort arrays slong with output

---
+++ Javascript/5_spread&sortArrays.js
@@ -0,0 +1,9 @@
+const number = [1, 2, 3, 4]; // An array
+const newNumber = [...number, 8, 7, 6, 5]; // "...number" takes values from "number" array and then get's appended to the numbers in "newNumber" array
+console.log(newNumber);
+
+console.log("----------------------------------------")
+
+const number2 = [1, 2, 3, 4]; 
+const newNumber2 = [number2, 8, 7, 6, 5]; // "number2" takes values from "number2" array and then get's appended to the numbers in "newNumber2" array as an array rather than continuation
+console.log(newNumber2);
